007faf2e469a3e395dba9079e1a5628c3f64edc5
liuz <8120826@qq.com>
2019-06-30 18:22:53 +0800

    day_02

--- src/main/java/net/kaipai/community/controller/AuthorizeController.java
+++ src/main/java/net/kaipai/community/controller/AuthorizeController.java
@@ -1,14 +1,29 @@
 package net.kaipai.community.controller;
 
+import net.kaipai.community.dto.AccessTokenDTO;
+import net.kaipai.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                             @RequestParam(name ="state") String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("5c191d3e1a5a9eb25536");
+        accessTokenDTO.setClient_secret("7f1e14dbe95180c0057149a251a4f61e991e5175");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        githubProvider.getAccessToken(accessTokenDTO);
         return "index";
 
     }
--- src/main/java/net/kaipai/community/provider/GithubProvider.java
+++ src/main/java/net/kaipai/community/provider/GithubProvider.java
@@ -1,26 +1,34 @@
 package net.kaipai.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import net.kaipai.community.dto.AccessTokenDTO;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class GithubProvider {
-    public String getAccessToken(AccessTokenDTO accessTokenDTO){
-        public static final MediaType JSON
-                = MediaType.get("application/json; charset=utf-8");
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
 
-        String post(String url, String json) throws IOException {
-            RequestBody body = RequestBody.create(JSON, json);
-            Request request = new Request.Builder()
-                    .url(url)
-                    .post(body)
-                    .build();
-            try (Response response = client.newCall(request).execute()) {
-                return response.body().string();
-            }
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            System.out.println(string);
+            return string;
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return null;
+
     }
 }
