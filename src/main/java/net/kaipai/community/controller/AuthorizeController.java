f247d6b1fcb72ae0529d90d512aace66d4f7e08b
a8120826 <8120826@qq.com>
2019-07-03 09:20:32 +0800

    day_03

--- src/main/java/net/kaipai/community/controller/AuthorizeController.java
+++ src/main/java/net/kaipai/community/controller/AuthorizeController.java
@@ -1,8 +1,10 @@
 package net.kaipai.community.controller;
 
 import net.kaipai.community.dto.AccessTokenDTO;
+import net.kaipai.community.dto.GithubUser;
 import net.kaipai.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,18 +14,27 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.Client.id}")
+    private String clientID;
+    @Value("${github.Client.secret}")
+    private String clientSecret;
+    @Value("${github.Redirect.uri}")
+    private String redirectUrl;
+
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                             @RequestParam(name ="state") String state){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("5c191d3e1a5a9eb25536");
-        accessTokenDTO.setClient_secret("7f1e14dbe95180c0057149a251a4f61e991e5175");
+        accessTokenDTO.setClient_id(clientID);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUrl);
         accessTokenDTO.setState(state);
-        githubProvider.getAccessToken(accessTokenDTO);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
 
     }
